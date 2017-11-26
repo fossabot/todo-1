53a2c3813458823446778f836bd0c0fcc0bf900e
Franklin Harding <franklinharding0.0@gmail.com>
2017-11-25 17:06:10 -0800

    Added store, postgres store, and postgres store tests

---
+++ store/postgres/postgres.go
@@ -0,0 +1,77 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+
+	gstore "github.com/fharding1/todo/store"
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+)
+
+type store struct {
+	db *sql.DB
+}
+
+type Options struct {
+	User, Pass string
+	Host       string
+	Port       int
+	DBName     string
+	SSLMode    string
+}
+
+func (o Options) connectionInfo() string {
+	return fmt.Sprintf("host='%s' port='%d' user='%s' password='%s' dbname='%s' sslmode='%s'",
+		o.Host, o.Port, o.User, o.Pass, o.DBName, o.SSLMode)
+}
+
+const todoTableCreationQuery = `
+CREATE TABLE IF NOT EXISTS todos (
+	id          SERIAL PRIMARY KEY,
+	description varchar(256),
+	createdAt   bigint,
+	completedAt bigint
+)`
+
+func New(options Options) (gstore.Service, error) {
+	db, err := sql.Open("postgres", options.connectionInfo())
+	if err != nil {
+		return nil, errors.Wrap(err, "connecting to postgres database")
+	}
+
+	_, err = db.Exec(todoTableCreationQuery)
+	if err != nil {
+		return nil, errors.Wrap(err, "creating todos table")
+	}
+
+	return &store{db: db}, nil
+}
+
+func (s *store) CreateTodo(todo gstore.Todo) (id int64, err error) {
+	err = s.db.QueryRow(
+		"INSERT INTO todos (description, createdAt, completedAt) VALUES ($1, $2, $3) RETURNING id",
+		todo.Description, todo.CreatedAt, todo.CompletedAt).Scan(&id)
+	return
+}
+
+func (s *store) GetTodo(id int64) (gstore.Todo, error) {
+	todo := gstore.Todo{ID: id}
+	err := s.db.QueryRow("SELECT description, createdAt, completedAt FROM todos WHERE id = $1", id).Scan(
+		&todo.Description, &todo.CreatedAt, &todo.CompletedAt)
+	if err == sql.ErrNoRows {
+		err = gstore.ErrNoResults
+	}
+	return todo, err
+}
+
+func (s *store) UpdateTodo(todo gstore.Todo) error {
+	_, err := s.db.Exec("UPDATE todos SET description = $1, createdAt = $2, completedAt = $3 WHERE id = $4",
+		todo.Description, todo.CreatedAt, todo.CompletedAt, todo.ID)
+	return err
+}
+
+func (s *store) DeleteTodo(id int64) error {
+	_, err := s.db.Exec("DELETE FROM todos WHERE id = $1", id)
+	return err
+}
--- store/store.go
+++ store/store.go
@@ -5,5 +5,16 @@ import "fmt"
 // ErrNoResults is a generic error of sql.ErrNoRows
 var ErrNoResults = fmt.Errorf("no results returned")
 
+type Todo struct {
+	ID          int64  `json:"id"`
+	Description string `json:"description"`
+	CreatedAt   int64  `json:"createdAt"`
+	CompletedAt *int64 `json:"completedAt,omitempty"` // nullable
+}
+
 type Service interface {
+	CreateTodo(todo Todo) (int64, error)
+	GetTodo(id int64) (Todo, error)
+	UpdateTodo(todo Todo) error
+	DeleteTodo(id int64) error
 }
