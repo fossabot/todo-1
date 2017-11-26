a18a871d63ac366fabc4261262ea05c2bb01ed4b
Franklin Harding <franklinharding0.0@gmail.com>
2017-11-26 01:51:18 -0800

    Fixed limitBody

--- server/server.go
+++ server/server.go
@@ -40,8 +40,7 @@ func (s *Server) deleteTodo(w http.ResponseWriter, r *http.Request) {}
 
 func limitBody(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// r.Body = http.MaxBytesReader(w, r, 8192)
-
+		r.Body = http.MaxBytesReader(w, r.Body, 1048576)
 		next.ServeHTTP(w, r)
 	})
 }
